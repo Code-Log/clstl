ec4b6401ff715433dcee835a8319beb1d21e63a9
Jaco Malan <jacomalan.12@gmail.com>
2019-09-07 10:53:57 +0200

    Fixed memory leak
    Signed-off-by: Jaco Malan <jacomalan.12@gmail.com>

--- include/clstl/string.h
+++ include/clstl/string.h
@@ -1,5 +1,6 @@
 #include <ostream>
 #include <clstl/types.h>
+#include <memory>
 
 namespace clstl {
 
@@ -15,9 +16,10 @@ namespace clstl {
         string(const string& other);
         ~string();
 
-        string& concat(const string& other);
-        string& operator+(const string& other);
+        string concat(const string& other);
+        string operator+(const string& other);
         char& operator[](ulong index);
+        void operator=(const string& other);
         const char* c_str() const;
 
     };
