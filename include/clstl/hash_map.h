7530e94b1d2e36242e702c5d8fe032d2d9bb4392
Jaco Malan <jacomalan.12@gmail.com>
2019-09-11 18:36:28 +0200

    Added hash_map

---
+++ include/clstl/hash_map.h
@@ -0,0 +1,67 @@
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#pragma once
+
+#include <clstl/array_list.h>
+#include <clstl/types.h>
+
+namespace clstl {
+
+    template<typename K, typename V>
+    struct hash_map {
+
+    private:
+        array_list<K> m_Keys;
+        array_list<V> m_Values;
+
+    public:
+
+        hash_map() = default;
+        hash_map(uint size) : m_Keys(array_list<K>(size)), m_Values(array_list<V>(size)) {}
+
+        void add(K key, V value) {
+
+            m_Keys.push(key);
+            m_Values.push(value);
+
+        }
+
+        void remove(const K& key) {
+
+            bool found = false;
+            uint i = 0;
+            while (!found && i < m_Keys.size()) {
+
+                if (m_Keys[i] == key)
+                    found = true;
+
+            }
+
+            m_Keys.splice(i, 1);
+            m_Values.splice(i, 1);
+
+        }
+
+        V& get(const K& key) {
+
+            bool found = false;
+            uint i = 0;
+            while (!found && i < m_Keys.size()) {
+
+                if (m_Keys[i] == key)
+                    found = true;
+
+            }
+
+            return m_Values[i];
+
+        }
+
+        V& operator[](const K& key) { return get(key); }
+
+    };
+
+}
+
+#endif
