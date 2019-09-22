1f84c4513c62f9c0d26d09cdc9949e47a36424bd
Jaco Malan <jacomalan.12@gmail.com>
2019-09-22 20:35:12 +0200

    Added slist
    Signed-off-by: Jaco Malan <jacomalan.12@gmail.com>

---
+++ include/clstl/slist.h
@@ -0,0 +1,70 @@
+namespace clstl {
+
+    template<typename T>
+    struct slist {
+
+    private:
+        T m_Data;
+        slist<T>* m_Next;
+    
+    public:
+        slist() : m_Next(nullptr) {}
+
+        slist(const T& data) : m_Data(data) {}
+
+        template<typename... args_t>
+        void emplace_back(args_t&&... args) {
+
+            slist<T>& last_item = this->last();
+            last_item.m_Next = new slist<T>(T(args...));
+
+        }
+
+        void push_back(const T& item) {
+
+            slist<T>& last_item = this->last();
+            last_item.m_Next = new slist<T>(item);
+
+        }
+
+        slist<T>& next() { return *m_Next; }
+
+        slist<T>& last() {
+
+            slist<T>* current = this;
+            while (current->m_Next != nullptr)
+                current = current->m_Next;
+
+            return *current;
+
+        }
+
+        uint size() {
+
+            uint ret = 1;
+            slist<T>* current = this;
+            while (current->m_Next != nullptr) {
+                current = current->m_Next;
+                ret++;
+            }
+
+            return ret;
+
+        }
+
+        T& get(uint index) { return (*this)[index]; }
+
+        T& operator[](uint index) {
+
+            slist<T>* current = this->m_Next;
+            for (int i = 0; i < index; i++) {
+                current = current->m_Next;
+            }
+
+            return current->m_Data;
+
+        }
+
+    };
+
+}
