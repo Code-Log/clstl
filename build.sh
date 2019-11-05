adddfddb84ced547981bf6f6c30a9bcbb6556247
Jaco Malan <jacomalan.12@gmail.com>
2019-11-05 09:38:28 +0200

    Changed CMAKE_INSTALL_PREFIX to /usr instead of /usr/local

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -d build ]; then
+    rm -rf build
+fi
+
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)
--- build_debug.sh
+++ build_debug.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -d build-debug ]; then
+    rm -rf build-debug
+fi
+
 mkdir build-debug
 cd build-debug
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)
