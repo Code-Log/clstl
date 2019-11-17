37f1acd860cbd4d2c3cfe69bdfbe45e1960b77f3
Jaco Malan <jacomalan.12@gmail.com>
2019-11-17 11:32:58 +0200

    Modified build script
    Signed-off-by: Jaco Malan <jacomalan.12@gmail.com>

--- build_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d build-debug ]; then
-    rm -rf build-debug
-fi
-
-mkdir build-debug
-cd build-debug
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)
