ca00366c444723c4ff9914e36ea1d8d98f55c7c4
Jaco Malan <jacomalan.12@gmail.com>
2020-07-15 18:53:52 +0200

    Added missing functions to string

--- include/clstl/string.h
+++ include/clstl/string.h
@@ -14,6 +14,9 @@ namespace clstl {
         unsigned int m_Length;
 
     public:
+
+        static const unsigned int npos = -1;
+
         string();
         string(const char* buffer);
         string(const string& other);
@@ -21,12 +24,54 @@ namespace clstl {
 
         string concat(const string& other) const;
         string operator+(const string& other) const;
+        string substr(unsigned int pos = 0, unsigned int count = npos) const;
+
+        char& at(unsigned int index);
         char& operator[](unsigned int index);
+
+        void clear();
+
+        unsigned int size() const;
+        unsigned int length() const;
+        unsigned int capacity() const;
+        unsigned int max_length() const { return npos; }
+
+        unsigned int find(char ch) const;
+        unsigned int rfind(char ch) const;
+
+        // Currently does nothing
+        void shrink_to_fit() { }
+        void reserve() { }
+
+        void push_back(char ch);
+        void pop_back();
+
         void operator=(const string& other);
+
+        char& front();
+        char& back();
+
+        char* data();
         const char* c_str() const;
+        
         bool operator==(const string& other) const;
         bool operator!=(const string& other) const;
+        
+        bool starts_with(const string& other) const;
+        bool starts_with(const char* other) const;
+        bool starts_with(char ch) const;
+
+        bool ends_with(const string& other) const;
+        bool ends_with(const char* other) const;
+        bool ends_with(char ch) const;
+        
+        string& operator+=(const string& other);
+        string& operator+=(const char* other);
+        string& operator+=(char ch);
+
         operator const char*() { return m_Buffer; }
+
+        bool empty() const;
 
     };
 
---
+++ tests/test_string.h
@@ -0,0 +1,8 @@
+#ifndef TEST_STRING_H
+#define TEST_STRING_H
+
+#pragma once
+
+void test_string();
+
+#endif
